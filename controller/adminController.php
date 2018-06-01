5835777de24ef415731f3c681066b6f03e75ed0b
nlrarojas <nlrarojas@hotmail.com>
2018-06-01 01:58:11 -0600

    Gestion de rutas

--- controller/adminController.php
+++ controller/adminController.php
@@ -13,10 +13,15 @@ class AdminController {
 
     public function invoke() {  
         if(isset($_GET['gestionarRutas'])) {
-            if (isset($_GET[''])){
-                
-            } 
-            include 'view/sitiosTuristicos.php';
+            if (isset($_GET['insertar'])){
+                include 'view/administrador/insertarRuta.php';
+            } elseif (isset($_GET['modificar'])){
+                include 'view/administrador/modificarRuta.php';
+            } elseif (isset($_GET['eliminar'])){
+                include 'view/administrador/eliminarRuta.php';
+            } else {
+                include 'view/administrador/verRutasTuristicas.php';
+            }
         } elseif(isset($_GET['gestionarSitios'])) {            
             if (isset($_GET['insertar'])){
                 include 'view/administrador/insertarSitio.php';
@@ -32,8 +37,9 @@ class AdminController {
                 } else {
                     include 'view/administrador/eliminarSitio.php';
                 }
+            } else {
+                include 'view/administrador/verSitios.php';
             }
-            include 'view/administrador/verSitios.php';
         } elseif(isset($_GET['cerrar'])) {            
             if (isset($_GET[''])){
                 
