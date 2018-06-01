a7564a87195ecec67395908033c6d81767b2b9e4
nlrarojas <nlrarojas@hotmail.com>
2018-05-31 18:26:00 -0600

    Parte de merlin

--- view/header.php
+++ view/header.php
@@ -34,7 +34,7 @@
                             <a class="nav-link btn btn-primary" href="index.php" style="color: white;">Inicio<span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item active">
-                            <a class="nav-link btn btn-primary" href="?sitiosTuristicos" style="color: white;">Sitios turiticos<span class="sr-only">(current)</span></a>
+                            <a class="nav-link btn btn-primary" href="?sitiosTuristicos" style="color: white;">Sitios turísticos<span class="sr-only">(current)</span></a>
                         </li>
                     </ul> 
                 </div>
